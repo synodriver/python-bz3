18ea231536674a12bf9818aa797dd743aca75d04
synodriver <diguohuangjiajinweijun@gmail.com>
2022-07-06 23:03:12 +0800

    bump version to 0.0.1

--- bz3/__init__.py
+++ bz3/__init__.py
@@ -1,7 +1,7 @@
 """
 Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
 """
-__version__ = "0.0.1.rc2"
+__version__ = "0.0.1"
 
 from bz3.backends import compress_file, crc32, decompress_file, test_file
 from bz3.bz3 import BZ3File, compress, decompress, open
