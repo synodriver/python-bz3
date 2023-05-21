4cebc4a9dacf8bbf48d394bea877c4129e5afbf7
synodriver <diguohuangjiajinweijun@gmail.com>
2023-05-21 17:26:17 +0800

    fix docstring

--- bz3/bz3.py
+++ bz3/bz3.py
@@ -295,11 +295,11 @@ def open(
     "ab" for binary mode, or "rt", "wt", "xt" or "at" for text mode.
     The default mode is "rb", and the default compresslevel is 9.
 
-    For binary mode, this function is equivalent to the BZ2File
-    constructor: BZ2File(filename, mode, compresslevel). In this case,
+    For binary mode, this function is equivalent to the BZ3File
+    constructor: BZ3File(filename, mode, ...). In this case,
     the encoding, errors and newline arguments must not be provided.
 
-    For text mode, a BZ2File object is created, and wrapped in an
+    For text mode, a BZ3File object is created, and wrapped in an
     io.TextIOWrapper instance with the specified encoding, error
     handling behavior, and line ending(s).
 
